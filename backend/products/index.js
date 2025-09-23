3992f5b5f030a7af0f000a032b2077562f071167
Krishmi <kuhkd150@gmail.com>
2025-09-23 21:08:12 +0530

    Information Exposure products fixed

--- backend/products/index.js
+++ backend/products/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const helmet = require("helmet");
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
@@ -6,23 +7,26 @@ const cors  = require('cors');
 require("dotenv").config(); 
 const checkoutRouter = require("./routes/checkout");
 
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8080;
 const mongo_url = process.env.MONGO_URL;
+
+// Use Helmet to add various security headers, including disabling X-Powered-By
+app.use(helmet());
 
 app.use(cors());
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader("Access-Control-Allow-Origin", "*");
   next();
 });
 
 mongoose.connect(mongo_url, {});
-const connection = mongoose.connection; 
+const connection = mongoose.connection;
 
 connection.once("open", () => {
   console.log("Database Connection Successful");
-})
+});
 
 const productRouter = require("./routes/ProductRoute");
 app.use("/product", productRouter);
@@ -37,5 +41,5 @@ app.use("/buyerReq", buyerReqRouter);
 app.use("/checkout", checkoutRouter);
 
 app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`)
-})
+  console.log(`Server is listening on port ${port}`);
+});
